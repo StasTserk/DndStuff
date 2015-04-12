cd98443045469daea1a68f8d4d2908e35b7c57a5
Stas <stas.tserkovny@gmail.com>
2015-04-12 17:19:22 -0600

    Fixed some errors in Spells.xml uncovered by the update

--- Views/SpellDetailView.cs
+++ Views/SpellDetailView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DnD5thEdTools.Views
 {
@@ -17,14 +18,15 @@ namespace DnD5thEdTools.Views
                 levelComposition += " " + cls;
             }
             return String.Format("*Name*: {0}\n\n" +
-                              "*School*: Level {1} {2}\n\n" +
+                              "*School*: {1} \n\n" +
+                              "*Level*: {2} \n\n" +
                               "*Casting Time*: {3}\n\n" +
                               "*Components*: {4}\n\n" +
                               "*Duration*: {5}\n\n" +
                               "*Concentration*: {6}\n\n" +
                               "*Range*: {7}\n\n" +
                               "*Targets*: {8}",
-                spell.Name, spell.Level, spell.School, spell.CastingTime, spell.Components,
+                spell.Name, spell.School, levelComposition, spell.CastingTime, spell.Components,
                 spell.Duration, spell.Concentration, spell.Range, spell.Targets);
         }
     }
